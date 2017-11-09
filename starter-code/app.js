2a2b0ab8340af3208c8caf7071eaeb11355093fc
Alejandro Roman <aromanarguello@gmail.com>
2017-11-08 19:58:45 -0500

    added some details

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,7 +8,7 @@ app.set('views', 'html-files');
 app.set('view engine', 'ejs');
 
 app.get( '/', (request, response, next) => {
-  response.render( 'home.ejs' );
+  response.render('home.ejs');
 });
 
 app.get( '/about', (request, response, next) => {
