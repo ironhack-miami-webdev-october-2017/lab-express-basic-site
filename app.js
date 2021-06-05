ea23ffc769b90a139fed3f533de9b4cef431dee5
Shirley Gianina <shirley.cd853f@gmail.com>
2021-06-05 18:03:29 +0200

    done

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ app.use(express.static('public'));
 // our first Route
 app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
  
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about-page.html'));
 
-app.get('/cat', (request, response, next) => response.sendFile(__dirname + '/views/cat-page.html'));
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works-page.html'));
+
+app.get('/gallery', (request, response, next) => response.sendFile(__dirname + '/views/gallery-page.html'));
+
 
    
 
