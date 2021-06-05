10dfd63accc3cbd15ea6fe26d90ddd7d452c90fe
Shirley Gianina <shirley.cd853f@gmail.com>
2021-06-05 17:22:44 +0200

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+ 
+// our first Route
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
+ 
+
+app.get('/cat', (request, response, next) => response.sendFile(__dirname + '/views/cat-page.html'));
+
+   
+
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
