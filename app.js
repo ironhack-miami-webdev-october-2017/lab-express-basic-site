971ff66ac223010dfd8eb54abcb8404b9afe6636
David Castillo <david@castillog.com>
2021-11-21 15:50:21 -0400

    Lab Done

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/index.html'));
+app.get('/about', (req, res, next) => res.sendFile(__dirname + '/views/about.html'));
+app.get('/works', (req, res, next) => res.sendFile(__dirname + '/views/works.html'));
+app.get('/gallery', (req, res, next) => res.sendFile(__dirname + '/views/gallery.html'));
+
+app.listen(3000,()=> console.log('Servidor Activo'));
